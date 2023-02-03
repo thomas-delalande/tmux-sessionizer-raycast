405da53d65c0a41a26f2d6730a82a8cac85dcd77
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-02-03 22:40:32 +0800

    feat: add preferences

--- src/tmux.tsx
+++ src/tmux.tsx
@@ -1,27 +1,44 @@
-import { Action, ActionPanel, closeMainWindow, Icon, List } from "@raycast/api";
+import { Action, ActionPanel, closeMainWindow, getPreferenceValues, Icon, List } from "@raycast/api";
 import { exec } from "child_process";
 import { readdirSync } from "fs";
 import { useEffect, useState } from "react";
 
+type Preferences = {
+  tmux: string;
+  terminal: {
+    path: string;
+  };
+  projectsDir1: string;
+  projectsDir2: string;
+};
+
 export default function Command() {
   const [projects, setProjects] = useState<string[]>([]);
-  const directory = "/Users/thomas-delalande/vgw/";
-  const tmux = "/opt/homebrew/bin/tmux";
+  const preferences = getPreferenceValues<Preferences>();
+  const tmuxPath = preferences.tmux;
+  const terminalPath = preferences.terminal.path;
+
   useEffect(() => {
-    const localProjects = readdirSync(directory, { withFileTypes: true })
-      .filter((dirent) => dirent.isDirectory())
-      .map((dirent) => dirent.name);
+    const projectDirs = [preferences.projectsDir1, preferences.projectsDir2];
+    console.log(preferences);
+    const localProjects = projectDirs.flatMap((directory) => {
+      if (directory === undefined) return [];
+      return readdirSync(directory, { withFileTypes: true })
+        .filter((dirent) => dirent.isDirectory())
+        .map((dirent) => `${directory}/${dirent.name}`);
+    });
     setProjects(localProjects);
   }, []);
 
   const selectProject = async (project: string) => {
+    const name = project.split("/").pop();
     try {
-      await run(`${tmux} new-session -A -d -s ${project} -c ${directory}/${project}`);
-      await run(`${tmux} switch -t ${project}`);
+      await run(`${tmuxPath} new-session -A -d -s ${name} -c ${project}`);
+      await run(`${tmuxPath} switch -t ${name}`);
     } catch (err) {
-      await run(`${tmux} switch -t ${project}`);
+      await run(`${tmuxPath} switch -t ${name}`);
     } finally {
-      await run(`open /Applications/Alacritty.app/`);
+      await run(`open ${terminalPath}`);
       closeMainWindow({
         clearRootSearch: true,
       });
@@ -45,7 +62,7 @@ export default function Command() {
       {projects.map((project, index) => (
         <List.Item
           key={index}
-          title={project}
+          title={project.split("/").pop() || project}
           actions={
             <ActionPanel>
               <ActionPanel.Section>
